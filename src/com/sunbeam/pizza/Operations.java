16158efaf1ac82f9d96d89ec1a33812778fa374c
Vaibhav_Jain <vaibhavjain7171@gmamil.com>
2025-02-22 17:23:43 +0530

    entry point layer

---
+++ src/com/sunbeam/pizza/Operations.java
@@ -0,0 +1,151 @@
+package com.sunbeam.pizza;
+
+import com.sunbeam.dao.AuthDAO;
+import com.sunbeam.dao.MenuDAO;
+import com.sunbeam.entities.Customer;
+import com.sunbeam.entities.ItemPrice;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Operations {
+    public int signIn(Scanner sc) throws SQLException {
+        String email, password;
+        System.out.print("Enter Email: ");
+        email = sc.next();
+        System.out.print("Enter Password: ");
+        password = sc.next();
+        try (AuthDAO adao = new AuthDAO()) {
+            Customer c = adao.signIn(email, password);
+            if (c == null) {
+                return -1;
+            } else {
+                if (c.getName().equals("admin") && c.getEmail().equals("admin@sunbeaminfo.com")) {
+                    System.out.println();
+                    System.out.println("Logged In as Admin!");
+                    System.out.println();
+                    return 1;
+                } else {
+                    System.out.println();
+                    System.out.println("Logged in as " + c.getName());
+                    System.out.println();
+                    return c.getId();
+                }
+            }
+        }
+    }
+
+    public void signUp(Scanner sc) throws SQLException {
+        Customer c = new Customer();
+        c.accept(sc);
+
+        try (AuthDAO adao = new AuthDAO()) {
+            adao.addCustomer(c);
+        }
+    }
+
+    public void getTypes() throws SQLException {
+        try (MenuDAO mdao = new MenuDAO()) {
+            mdao.fetchTypes();
+        }
+    }
+
+    public void getItems(String type) throws SQLException {
+        try (MenuDAO mdao = new MenuDAO()) {
+            mdao.fetchItems(type);
+        }
+    }
+
+    public int getSizes(Scanner sc) throws SQLException {
+        int itemId;
+        System.out.print("Enter Item ID: ");
+        itemId = sc.nextInt();
+        try (MenuDAO mdao = new MenuDAO()) {
+            mdao.fetchSizes(itemId);
+        }
+        return itemId;
+    }
+
+    private int ItemMenu(Scanner sc) {
+        System.out.println("1. Veg Items");
+        System.out.println("2. Non-Veg Items");
+        System.out.print("Enter your choice: ");
+        return sc.nextInt();
+    }
+
+    public ItemPrice getItemPrice(Scanner sc) throws SQLException {
+        int itemChoice, priceId;
+        itemChoice = ItemMenu(sc);
+        switch (itemChoice) {
+            case 1: {
+                getItems("Veg");
+                getSizes(sc);
+                System.out.print("Enter Price ID: ");
+                priceId = sc.nextInt();
+                try (MenuDAO mdao = new MenuDAO()) {
+                    return mdao.fetchItemPrice(priceId);
+                }
+            }
+            case 2: {
+                getItems("NonVeg");
+                getSizes(sc);
+                System.out.print("Enter Price ID: ");
+                priceId = sc.nextInt();
+                try (MenuDAO mdao = new MenuDAO()) {
+                    return mdao.fetchItemPrice(priceId);
+                }
+            }
+            default:
+                System.out.println("Wrong Choice. Try Again!");
+                break;
+        }
+        System.out.println("Back to previous menu.");
+        return null;
+    }
+
+    public void addOrder(int customerId, List<ItemPrice> cart) throws SQLException {
+        if (cart.size() < 1) {
+            System.out.println("Cart is Empty!");
+            return;
+        }
+
+        try (MenuDAO mdao = new MenuDAO()) {
+//			mdao.saveOrder(customerId);
+            mdao.saveOrderDetails(cart, customerId);
+            System.out.println("Order Placed!");
+        }
+    }
+
+    public void getOrders() throws SQLException {
+        System.out.println();
+        try (MenuDAO mdao = new MenuDAO()) {
+            mdao.fetchOrders();
+        }
+        System.out.println();
+    }
+
+    public void getOrderDetails(Scanner sc) throws SQLException {
+        int orderId;
+        System.out.print("Enter Order ID: ");
+        orderId = sc.nextInt();
+        System.out.println();
+        try (MenuDAO mdao = new MenuDAO()) {
+            mdao.fetchOrderDetails(orderId);
+        }
+        System.out.println();
+    }
+
+    public void getCart(List<ItemPrice> cart) throws SQLException {
+        if (cart.isEmpty()) {
+            System.out.println("Cart is Empty!");
+            return;
+        }
+        int total = 0;
+        for (ItemPrice ip : cart) {
+            total += ip.getPrice();
+            System.out.println(ip);
+        }
+        System.out.println("Total Amount: " + total);
+    }
+}
---
+++ src/com/sunbeam/pizza/Shop.java
@@ -0,0 +1,122 @@
+package com.sunbeam.pizza;
+
+import com.sunbeam.entities.ItemPrice;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Shop {
+    private static int AuthMenu(Scanner sc) {
+        System.out.println("0. EXIT");
+        System.out.println("1. Sign In");
+        System.out.println("2. Sign Up");
+        System.out.print("Enter your choice: ");
+        return sc.nextInt();
+    }
+
+    private static int CustomerMenu(Scanner sc) {
+        System.out.println("1. Show Veg Items");
+        System.out.println("2. Show Non-Veg Items");
+        System.out.println("3. Show Available Sizes");
+        System.out.println("4. Add to Cart");
+        System.out.println("5. Show Cart");
+        System.out.println("6. Place Order");
+        System.out.println("7. Sign Out");
+        System.out.print("Enter your choice: ");
+        return sc.nextInt();
+    }
+
+    private static int AdminMenu(Scanner sc) {
+        System.out.println("1. Show All Orders");
+        System.out.println("2. Show Order Details");
+        System.out.println("3. Sign Out");
+        System.out.print("Enter your choice: ");
+        return sc.nextInt();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Pizza Shop!");
+        int authChoice, adminChoice, customerChoice;
+        int authStatus;
+        List<ItemPrice> cart = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        Operations op = new Operations();
+
+        try {
+            while ((authChoice = AuthMenu(sc)) != 0) {
+                switch (authChoice) {
+                    case 1: {
+                        authStatus = op.signIn(sc);
+                        if (authStatus == 1) {
+                            while ((adminChoice = AdminMenu(sc)) != 3) {
+                                switch (adminChoice) {
+                                    case 1:
+                                        op.getOrders();
+                                        break;
+                                    case 2:
+                                        op.getOrderDetails(sc);
+                                        break;
+                                    default:
+                                        System.out.println("Wrong Choice. Try Again!");
+                                        break;
+                                }
+                            }
+                            System.out.println("Admin Signed Out!");
+                        } else if (authStatus != -1) {
+                            while ((customerChoice = CustomerMenu(sc)) != 7) {
+                                switch (customerChoice) {
+                                    case 1:
+                                        op.getItems("Veg");
+                                        break;
+                                    case 2:
+                                        op.getItems("NonVeg");
+                                        break;
+                                    case 3:
+                                        op.getSizes(sc);
+                                        break;
+                                    case 4:
+                                        cart.add(op.getItemPrice(sc));
+                                        System.out.println();
+                                        System.out.println("Item Added!");
+                                        System.out.println();
+                                        break;
+                                    case 5:
+                                        System.out.println();
+                                        op.getCart(cart);
+                                        System.out.println();
+                                        break;
+                                    case 6:
+                                        op.addOrder(authStatus, cart);
+                                        cart.clear();
+                                        break;
+                                    default:
+                                        System.out.println("Wrong Choice. Try Again!");
+                                        break;
+                                }
+                            }
+                            System.out.println("Customer Signed Out!");
+                        } else {
+                            System.out.println("Sign In Failed!");
+                        }
+                    }
+                    break;
+                    case 2:
+                        op.signUp(sc);
+                        break;
+                    default:
+                        System.out.println("Wrong Choice. Try Again!");
+                        break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+        System.out.println("Thank You for visiting!");
+        System.out.println();
+    }
+}
